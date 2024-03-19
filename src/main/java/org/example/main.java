009c112a3870a5c209ffdf963f9d8cfb72d16d0f
Jan Jasa <jan_jasa1@baylor.edu>
2024-03-19 12:49:25 -0500

    TestCommit

--- src/main/java/org/example/main.java
+++ src/main/java/org/example/main.java
@@ -5,5 +5,6 @@ package org.example;
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("TESTING IF THIS IS ADDED");
     }
 }
