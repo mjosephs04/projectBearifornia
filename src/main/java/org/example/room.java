4c0ffae6d58843719d7b5e5b0f355fd3a69cbc5e
Jan Jasa <jan_jasa1@baylor.edu>
2024-03-24 12:06:02 -0500

    Added printRoomInfo method for room.

--- src/main/java/org/example/room.java
+++ src/main/java/org/example/room.java
@@ -1,21 +1,47 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 //jan
 
 public class room {
     private Double cost = 0.00;
     private Integer roomNumber, numOfBeds, qualityLevel = 0;
-    private String typeOfRoom = "";
+    private String typeOfRoom, bedType = "";
     private boolean smokingAllowed = false;
 
+    //DEFAULT CONSTRUCTOR
+    public room(){}
+
     //CONSTRUCTOR
-    public room(Double c, Integer roomNum, Integer numBed, Integer quality, String roomType, boolean smoking){
+    public room(Double c, Integer roomNum, Integer numBed, Integer quality, String roomType, boolean smoking) {
         this.cost = c;
         this.roomNumber = roomNum;
         this.numOfBeds = numBed;
         this.qualityLevel = quality;
         this.typeOfRoom = roomType;
         this.smokingAllowed = smoking;
+    }
+
+
+    //PRINT INFO
+    public void printRoomInfo(){
+        System.out.println(roomNumber);
+        System.out.println("Room type: " + typeOfRoom);
+        System.out.println("Bed type: " + bedType);
+        System.out.println("# of beds: " + numOfBeds);
+        System.out.println("Quality level: " + qualityLevel);
+        if(!smokingAllowed){
+            System.out.println("Smoking not allowed.");
+        }
+        else{
+            System.out.println("Smoking allowed.");
+        }
     }
 
     //GETTERS
@@ -57,6 +83,8 @@ public class room {
     public void setSmokingStatus(boolean b){
         this.smokingAllowed = b;
     }
+
+
 
     @Override
     public boolean equals(Object obj){
