0bc6360dd66b4bd3bc9d1dcabcea6fe3ed676943
cateburglar <91097132+cateburglar@users.noreply.github.com>
2024-03-24 14:51:15 -0500

    added barebones functions and variables to user

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,5 +1,21 @@
 package org.example;
 
 public interface User {
+    String name = "default";
+    Integer idNumber = -1;
+
+    //reserves a room and modifies csv accordingly,
+    // returns a String with a confirmation/error message
+    public String reserveRoom();
+
+    //returns the name of the user
+    public String getName();
+
+    //returns whether it is an admin, clerk, or guest
+    public User getType();
+
+
+    //returns id number
+    public Integer getIdNumber();
 
 }
