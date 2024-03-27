72c0b44fe4cc36a702a3e288d87f67e4094aa318
markjosephs <mjosephs@customsportssleeves.com>
2024-03-27 13:05:49 -0500

    adding frontend(react) and backend(springboot) directories

---
+++ spring-boot/src/main/java/com/example/springboot/MyController.java
@@ -0,0 +1,48 @@
+package com.example.springboot;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.swing.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api")
+public class MyController {
+
+    @RequestMapping("/runFunction")
+    public List<room> convertCSV() {
+
+        List<room> listOfRooms = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(
+                new FileReader("./RoomsAvailable.csv"))) {
+            String line;
+            br.readLine();
+            while ((line = br.readLine()) != null) {
+                String[] row = line.split(",");
+                room nRoom = new room(Double.parseDouble(row[1]), Integer.parseInt(row[0]), Integer.parseInt(row[3]), Integer.parseInt(row[4]), row[2], Boolean.parseBoolean(row[6]));
+                listOfRooms.add(nRoom);
+            }
+        }catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return listOfRooms;
+    }
+
+    @PostMapping("/endpoint")
+    public ResponseEntity<String> handlePostRequest(@RequestBody String requestBody) {
+        System.out.println(requestBody);
+        return ResponseEntity.ok("Request received successfully");
+    }
+
+    @GetMapping("/img")
+    public ResponseEntity<String> getImageLink() {
+        // Replace this with the actual URL or path to your image
+        String imageUrl = "https://www.grandbearresort.com/images/slider/slider-2.jpg";
+
+        return ResponseEntity.ok(imageUrl);
+    }
+}
---
+++ spring-boot/src/main/java/com/example/springboot/room.java
@@ -0,0 +1,98 @@
+package com.example.springboot;
+
+//jan
+
+public class room {
+    private Double cost = 0.00;
+    private Integer roomNumber, numOfBeds, qualityLevel = 0;
+    private String typeOfRoom, bedType = "";
+    private boolean smokingAllowed = false;
+
+    //DEFAULT CONSTRUCTOR
+    public room(){}
+
+    //CONSTRUCTOR
+    public room(Double c, Integer roomNum, Integer numBed, Integer quality, String roomType, boolean smoking) {
+        this.cost = c;
+        this.roomNumber = roomNum;
+        this.numOfBeds = numBed;
+        this.qualityLevel = quality;
+        this.typeOfRoom = roomType;
+        this.smokingAllowed = smoking;
+    }
+
+
+    //PRINT INFO
+    public void printRoomInfo(){
+        System.out.println(roomNumber);
+        System.out.println("Room type: " + typeOfRoom);
+        System.out.println("Bed type: " + bedType);
+        System.out.println("# of beds: " + numOfBeds);
+        System.out.println("Quality level: " + qualityLevel);
+        if(!smokingAllowed){
+            System.out.println("Smoking not allowed.");
+        }
+        else{
+            System.out.println("Smoking allowed.");
+        }
+    }
+
+    //GETTERS
+    public Double getCost(){
+        return this.cost;
+    }
+    public Integer getRoomNumber(){
+        return this.roomNumber;
+    }
+    public Integer getNumOfBeds(){
+        return this.numOfBeds;
+    }
+    public Integer getQualityLevel(){
+        return this.qualityLevel;
+    }
+    public String getTypeOfRoom(){
+        return this.typeOfRoom;
+    }
+    public boolean getSmokingStatus(){
+        return this.smokingAllowed;
+    }
+    public String getBedType(){return this.bedType;}
+
+    //SETTERS
+    public void setCost(Double c){
+        this.cost = c;
+    }
+    public void setRoomNumber(Integer num){
+        this.roomNumber = num;
+    }
+    public void setNumOfBeds(Integer num){
+        this.numOfBeds = num;
+    }
+    public void setQualityLevel(Integer num){
+        this.qualityLevel = num;
+    }
+    public void setTypeOfRoom(String s){
+        this.typeOfRoom = s;
+    }
+    public void setSmokingStatus(boolean b){
+        this.smokingAllowed = b;
+    }
+    public void setBedType(String s){this.bedType = s;}
+
+
+    @Override
+    public boolean equals(Object obj){
+        if(obj instanceof room){
+            return ((room) obj).getRoomNumber().equals(roomNumber) &&
+                    ((room) obj).getNumOfBeds().equals(numOfBeds) &&
+                    ((room) obj).getQualityLevel().equals(qualityLevel) &&
+                    ((room) obj).getCost().equals(cost) &&
+                    ((room) obj).getTypeOfRoom().equals(typeOfRoom) &&
+                    ((room) obj).getBedType().equals(bedType);
+
+        }
+        else{
+            return false;
+        }
+    }
+}
