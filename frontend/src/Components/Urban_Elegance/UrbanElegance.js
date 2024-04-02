6722638067fa86ab83d71d941dd0e7341ae843d8
Jacob Celis <jacob.gokale@gmail.com>
2024-04-01 23:09:40 -0500

    Urban Elegance Changes

--- frontend/src/Components/Urban_Elegance/UrbanElegance.js
+++ frontend/src/Components/Urban_Elegance/UrbanElegance.js
@@ -1,14 +1,58 @@
 
-import React from 'react'
-import Layout from '../Layout/Layout.js'
-import Component from "../../Component";
+import Layout from '../Layout/Layout'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+import "./UrbanElegance.css"
 const UrbanElegance = () =>{
+
+    // State to store the fetched data
+    const [listingData, setListingData] = useState([]);
+
+
+    // Function to fetch data from the API
+    const fetchData = () => {
+        axios.get('http://localhost:8080/rooms/urban-elegance')
+            .then(response => {
+                if (response.status !== 200) {
+                    throw new Error('Network response was not ok: ' + response.data);
+                }
+                return response.data;
+            })
+            .then(data => {
+                console.log(data);
+                setListingData(data);
+            })
+            .catch(error => {
+                console.error('There was a problem with the fetch operation:', error);
+            });
+    };
+
+    // Fetch data when the component mounts
+    useEffect(() => {
+        fetchData();
+    }, []); // Empty dependency array ensures useEffect runs only once on mount
 
     return (
         <div>
-            <Layout />
-            <Component />
-            <div>HELLO WORLD</div>
+            <Layout/>
+
+            <div className='listing-name'>
+                {listingData.name}
+            </div>
+
+            <div className='listing-desc'>
+                {listingData.description}
+            </div>
+
+            {/*<div className='listing-cost'>*/}
+            {/*    {listingData.cost}*/}
+            {/*</div>*/}
+
+            <div className='listing-img'>
+                <img className='listing-img' src={listingData.imageURL}></img>
+            </div>
+
+
         </div>
     );
 
