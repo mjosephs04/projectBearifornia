79b5f4bc8b82e3d6b7609317b9807a0e4c0f9955
Jan Jasa <jan_jasa1@baylor.edu>
2024-04-03 17:55:29 -0500

    initialized clerk center page

---
+++ frontend/src/Components/Clerk_Center/ClerkCenter.js
@@ -0,0 +1,37 @@
+
+import Layout from '../Layout/Layout'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+import "./ClerkCenter.css"
+import bedroomButton from "../../assets/76244.png";
+import {Link} from "react-router-dom";
+const ClerkCenter = (props) =>{
+    const [usernameError, setEmailError] = useState('')
+    const [passwordError, setPasswordError] = useState('')
+
+    const onButtonClick = () => {
+        //PUT THE LOGIN METHOD HERE MY BOAAAAA
+    }
+
+    return (
+        <div>
+            <Layout/>
+            <div className='pageTitle'>
+                <h1>Clerk Center</h1>
+            </div>
+            <br/>
+            <div className='inputContainer'>
+                <p>Add Room</p>
+            </div>
+            <div className='button'>
+                <Link to='/add-room'>
+                    <img className='bedroomButton' src={bedroomButton}/>
+                </Link>
+            </div>
+
+        </div>
+    );
+
+}
+
+export default ClerkCenter
