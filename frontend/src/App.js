dcfd2348f598c39517ce0e1b31e8999665fd0337
Jan Jasa <jan_jasa1@baylor.edu>
2024-04-02 19:32:36 -0500

    setup loginpage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,8 @@ import Reserve from './Components/Reserve/Reserve'
 import UrbanElegance from './Components/Urban_Elegance/UrbanElegance'
 import NatureRetreat from './Components/Nature-Retreat/Nature-Retreat'
 import Vintage from './Components/Vintage/Vintage'
+import LoginPage from './Components/Login_Page/LoginPage'
 import {Route, Routes} from "react-router-dom";
-
 const App = () => {
     return (
         <>
@@ -16,6 +16,7 @@ const App = () => {
                 <Route exact path='/reserve/urban-elegance' element={<UrbanElegance />}/>
                 <Route exact path='/reserve/nature-retreat' element={<NatureRetreat />}/>
                 <Route exact path='/reserve/old-vintage' element={<Vintage />}/>
+                <Route exact path='/login-page' element={<LoginPage />}/>
             </Routes>
         </>
     );
