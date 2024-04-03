f177b2961040baaf7e4433bc0682d9694cbfcb85
markjosephs <mjosephs@customsportssleeves.com>
2024-04-02 20:57:34 -0500

    date selector and fixing listing img

--- spring-boot/src/main/java/com/example/springboot/Listing.java
+++ spring-boot/src/main/java/com/example/springboot/Listing.java
@@ -47,5 +47,14 @@ public class Listing implements Serializable {
     }
 
 
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+
 }
 
