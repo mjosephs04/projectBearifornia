a5f46f9335fc1e72262fa973e62b29b004719fcd
cateburglar <91097132+cateburglar@users.noreply.github.com>
2024-04-03 18:03:54 -0500

    implemented basic functions in admin class

--- spring-boot/src/main/java/springboot/Admin.java
+++ spring-boot/src/main/java/springboot/Admin.java
@@ -1,28 +1,37 @@
 package springboot;
 
 public class Admin implements User{
+    private static UserType classification = UserType.ADMIN;
+    private String name;
+    private Integer idNumber;
+
+
+    public Admin(String name, Integer id){
+        this.name = name;
+        this.idNumber = id;
+    }
     @Override
     public UserType getType() {
-        return null;
+        return classification;
     }
 
     @Override
     public String getName() {
-        return null;
+        return name;
     }
 
     @Override
     public void setName(String x) {
-
+        name = x;
     }
 
     @Override
     public Integer getIdNumber() {
-        return null;
+        return idNumber;
     }
 
     @Override
     public void setIdNumber(Integer id) {
-
+        idNumber = id;
     }
 }
