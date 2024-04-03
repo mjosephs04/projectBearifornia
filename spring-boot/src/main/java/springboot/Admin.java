0dfee6367c40900e88ee876115ced7600594585d
cateburglar <91097132+cateburglar@users.noreply.github.com>
2024-04-03 18:10:10 -0500

    added an addUser function to Admin

--- spring-boot/src/main/java/springboot/Admin.java
+++ spring-boot/src/main/java/springboot/Admin.java
@@ -1,5 +1,9 @@
 package springboot;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Admin implements User{
     private static UserType classification = UserType.ADMIN;
     private String name;
@@ -33,5 +37,49 @@ public class Admin implements User{
     @Override
     public void setIdNumber(Integer id) {
         idNumber = id;
+    }
+
+    public String addUser(String username, String password, UserType type) {
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("spring-boot/src/main/resources/Users.csv"));
+        ) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Failed to read in the currently available rooms";
+        }
+
+        // Add the new line containing the new room
+        StringBuilder newUser = new StringBuilder();
+        newUser.append(username).append(",").append(password).append(",").append(type.toString());
+        if (!lines.contains(newUser)) {
+            lines.add(newUser.toString());
+        } else {
+            return "User already exists";
+        }
+
+        FileWriter fw;
+        try {
+            fw = new FileWriter("spring-boot/src/main/resources/Users.csv");
+        } catch (IOException x) {
+            x.printStackTrace();
+            return "Could not write to User database";
+        }
+
+        // Write the updated content back to the CSV file
+        try (BufferedWriter writer = new BufferedWriter(fw)) {
+            for (String line : lines) {
+                writer.write(line);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Could not write to User database";
+        }
+
+        return "success";
     }
 }
