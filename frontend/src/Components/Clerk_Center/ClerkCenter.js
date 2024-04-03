c8e6b3327f7e363ca71df26183df44b36ff3a7de
Jan Jasa <jan_jasa1@baylor.edu>
2024-04-03 18:45:02 -0500

    fixed styling

--- frontend/src/Components/Clerk_Center/ClerkCenter.js
+++ frontend/src/Components/Clerk_Center/ClerkCenter.js
@@ -4,6 +4,7 @@ import React, {useEffect, useState} from 'react'
 import axios from 'axios'
 import "./ClerkCenter.css"
 import bedroomButton from "../../assets/76244.png";
+import checkinoutButton from "../../assets/95145-200.png"
 import {Link} from "react-router-dom";
 const ClerkCenter = (props) =>{
     const [usernameError, setEmailError] = useState('')
@@ -20,15 +21,20 @@ const ClerkCenter = (props) =>{
                 <h1>Clerk Center</h1>
             </div>
             <br/>
-            <div className='inputContainer'>
+            <div className='inputContainer1'>
                 <p>Add Room</p>
+            </div>
+            <div className='inputContainer2'>
+                <p>Check In/Out</p>
             </div>
             <div className='button'>
                 <Link to='/add-room'>
                     <img className='bedroomButton' src={bedroomButton}/>
                 </Link>
             </div>
-
+            <div className='button'>
+                <img className='bedroomButton' src={checkinoutButton}/>
+            </div>
         </div>
     );
 
