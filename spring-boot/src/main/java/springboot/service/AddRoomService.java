b3a07bfd97a4ab934551f058f29e20b91caac482
cateburglar <91097132+cateburglar@users.noreply.github.com>
2024-04-04 01:42:05 -0500

    just re-assigned responsibilities

--- spring-boot/src/main/java/springboot/service/AddRoomService.java
+++ spring-boot/src/main/java/springboot/service/AddRoomService.java
@@ -1,9 +1,9 @@
 package springboot.service;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -23,6 +23,51 @@ public class AddRoomService {
         } catch (IOException e) {
             throw new IOException("Failed to add room to CSV", e);
         }
+    }
+
+    //takes a csv formatted string with the info for a room and inserts it into the
+    // database of available rooms
+    //returns either "success" or a string containing "failure" depending on result
+    public String addRoom2(String newRoom) {
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("spring-boot/src/main/resources/Rooms.csv"));
+        ) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Failed to read in the currently available rooms";
+        }
+
+        // Add the new line containing the new room
+        if (!lines.contains(newRoom)) {
+            lines.add(newRoom);
+        } else {
+            return "Room is already there";
+        }
+
+        FileWriter fw;
+        try {
+            fw = new FileWriter("spring-boot/src/main/resources/Rooms.csv");
+        } catch (IOException x) {
+            x.printStackTrace();
+            return "Could not write to RoomsAvailable database";
+        }
+
+        // Write the updated content back to the CSV file
+        try (BufferedWriter writer = new BufferedWriter(fw)) {
+            for (String line : lines) {
+                writer.write(line);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Could not write to Rooms database";
+        }
+
+        return "success";
     }
 }
 
