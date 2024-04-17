3bf250fc3c992a7fcadd49f551f3e9f585518ec4
Jacob Celis <jacob.gokale@gmail.com>
2024-04-17 18:20:37 -0500

    Adding Product Details page

---
+++ frontend/src/Components/ProductDetails/ProductDetails.js
@@ -0,0 +1,103 @@
+import Layout from '../Layout/Layout'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+import "./ProductDetails.css"
+import {Link} from "react-router-dom";
+
+// Assets
+import goldChainIMG from '../../assets/GoldChain.png'
+import sunglassesIMG from '../../assets/Sunglasses.png'
+import toothbrushIMG from '../../assets/Toothbrush.png'
+import toothpasteIMG from '../../assets/Toothpaste.png'
+import playstationIMG from '../../assets/Playstation.png'
+const ProductDetails = (props) =>{
+
+    // State to store the fetched data
+    const [productListingData, setproductListingData] = useState([]);
+
+
+    const fetchData = () => {
+        axios.get('http://localhost:8080/rooms/nature-retreat') // Have to change the axios.get ask Mark about this
+            .then(response => {
+                if (response.status !== 200) {
+                    throw new Error('Network response was not ok: ' + response.data);
+                }
+                return response.data;
+            })
+            .then(data => {
+                setproductListingData(data);
+            })
+            .catch(error => {
+                console.error('There was a problem with the fetch operation:', error);
+            });
+    };
+    // Fetch data when the component mounts
+    useEffect(() => {
+        fetchData();
+    }, []); // Empty dependency array ensures useEffect runs only once on mount
+
+
+
+
+    // This will link the functionality of adding an item to the cart
+    // API Call
+    const onButtonClick = () => {
+
+    };
+
+
+    return (
+        <div class="Container1">
+            <Layout/>
+
+            <div className="image-container">
+                <img src={playstationIMG}></img>
+
+                {/*This is what will grab the data from the backend*/}
+                {/*<img src={productListingData.imageURL}></img>*/}
+
+                <button>Add To Cart</button>
+            </div>
+
+
+            <Link to='/shop-catalog'>
+                <button className='Back-Button'>Go Back</button>
+            </Link>
+
+            <div className="name-container">
+                <p>Name of Product: API call to get product name</p>
+
+                {/*This is what will grab the data from the backend*/}
+                {/*{productListingData.productName}*/}
+            </div>
+
+
+            <div className="availability-container">
+                <p>Items available: API call to get accurate count</p>
+
+                {/*This is what will grab the data from the backend*/}
+                {/*{productListingData.productAvailability}*/}
+            </div>
+
+
+            <div className="price-container">
+                <p>$$$</p>
+
+                {/*This is what will grab the data from the backend*/}
+                {/*{productListingData.productPrice}*/}
+            </div>
+
+            <div className="description-container">
+                <p>This will be an api call from the back end to get the correct description for the item</p>
+                {/*This is what will grab the data from the backend*/}
+                {/*{productListingData.productDescription}*/}
+            </div>
+
+
+        </div>
+
+    );
+
+}
+
+export default ProductDetails
--- frontend/src/Components/Shop_Catalog/ShopCatalog.js
+++ frontend/src/Components/Shop_Catalog/ShopCatalog.js
@@ -21,27 +21,38 @@ const ShopCatalog = () =>{
             <div className="image-container">
                 <img src={goldChainIMG} alt="Image 1"/>
                 <p>Tupac's Gold Chain</p>
-                <button>More Details</button>
+                <Link to='/product-details'>
+                    <button>More Details</button>
+                </Link>
             </div>
             <div className="image-container">
                 <img src={sunglassesIMG} alt="Image 2"/>
                 <p>Sunglasses</p>
-                <button>More Details</button>
+                <Link to='/product-details'>
+                    <button>More Details</button>
+                </Link>
             </div>
             <div className="image-container">
                 <img src={toothbrushIMG} alt="Image 3"/>
                 <p>Philips Sonicare Toothbrush</p>
-                <button>More Details</button>
+                <Link to='/product-details'>
+                    <button>More Details</button>
+                </Link>
             </div>
             <div className="image-container">
                 <img src={toothpasteIMG} alt="Image 4"/>
                 <p>Generic Toothpaste</p>
-                <button>More Details</button>
+                <Link to='/product-details'>
+                    <button>More Details</button>
+                </Link>
             </div>
             <div className="image-container">
                 <img src={playstationIMG} alt="Image 4"/>
                 <p>Playstation 5 (Slim)</p>
-                <button>More Details</button>
+
+                <Link to='/product-details'>
+                    <button>More Details</button>
+                </Link>
             </div>
         </div>
     );
