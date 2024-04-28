1be5659a1087e2ad84d2dae5e77a68c50f96bd68
Jan Jasa <janjasa03@gmail.com>
2024-04-27 21:53:59 -0500

    redesigned reset password page

--- frontend/src/Components/Admin_Reset_User/AdminReset.js
+++ frontend/src/Components/Admin_Reset_User/AdminReset.js
@@ -52,40 +52,29 @@ const AdminReset = (props) =>{
     return (
         <div>
             <Layout/>
-            <br/>
-            <div className='inputContainer'>
-
-                <input
-                    //value={email}
-                    placeholder="Enter your username here"
-                    className={'inputBox'}
-                    onChange={evt => setUsername(evt.target.value)}
-                />
-            </div>
-            <br/>
-            <div className='inputContainer'>
-                <input
-                    //value={email}
-                    placeholder="Enter your password here"
-                    className={'inputBox'}
-                    type="password"
-                    onChange={evt => setPassword(evt.target.value)}
-                />
-            </div>
-            <br/>
-            <div className={'inputContainer'}>
-                <input className={'inputButton'} type="button" onClick={onButtonClick} value={'Log in'}/>
-            </div>
-            <div className={'inputContainer'}>
-                <Link to='/create-account'>
-                    <input className={'inputButton'} type="button2"  value={'New Account'}/>
-                </Link>
+            <div className='box'>
+                <div className='inputContainerReset'>
+                    <h1>Admin's Reset PassTool</h1>
+                    <input
+                        //value={email}
+                        placeholder="Enter guest username here"
+                        className={'inputBox'}
+                        onChange={evt => setUsername(evt.target.value)}
+                    />
+                    <input
+                        //value={email}
+                        placeholder="Enter new password here"
+                        className={'inputBox'}
+                        type="password"
+                        onChange={evt => setPassword(evt.target.value)}
+                    />
+                    <input className={'inputButton'} type="button" onClick={onButtonClick} value={'Log in'}/>
+                </div>
             </div>
 
             {error && (
                 <h1 className='login-failed'>Login Failed: Username or Password was incorrect</h1>
             )}
-
 
 
         </div>
