fd21c7a0d3f9375ca37ffe44a4f4fed9db620936
Oliver Liu <oliverliu100@gmail.com>
2024-04-30 17:09:59 -0500

    Add Products table

--- spring-boot/src/main/java/springboot/Application.java
+++ spring-boot/src/main/java/springboot/Application.java
@@ -2,13 +2,13 @@ package springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springboot.database.Setup;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        databaseSetup.initialize();
-
+        Setup.initialize();
         SpringApplication.run(Application.class, args);
     }
 }
