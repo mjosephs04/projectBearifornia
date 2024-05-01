78381d37124e9cd4221cdcd741715018a7d5e08b
Oliver Liu <oliverliu100@gmail.com>
2024-04-30 23:53:07 -0500

    Add image URL attribute to Product

--- spring-boot/src/main/java/springboot/Product.java
+++ spring-boot/src/main/java/springboot/Product.java
@@ -15,68 +15,62 @@ public class Product {
     private int productStock;
     private String productDescription;
     private double productPrice;
+    private String imageURL;
 
-    public Product(String productId, String productName, int productStock, String productDescription, double productPrice) {
+    public Product(String productId, String productName, int productStock, String productDescription, double productPrice, String imageURL) {
         this.productId = productId;
         this.productName = productName;
         this.productStock = productStock;
         this.productDescription = productDescription;
         this.productPrice = productPrice;
+        this.imageURL = imageURL;
     }
 
-
-    public static List<Product> getProductsFromDatabase() throws SQLException {
-        List<Product> products = new ArrayList<>();
-        String query = "SELECT * FROM PRODUCTS";
-        try (Connection conn = Setup.getDBConnection();
-             PreparedStatement stmt = conn.prepareStatement(query);
-             ResultSet rs = stmt.executeQuery()) {
-            while (rs.next()) {
-                String productId = rs.getString("PRODUCTID");
-                String productName = rs.getString("PRODUCTNAME");
-                int productStock = rs.getInt("PRODUCTSTOCK");
-                String productDescription = rs.getString("PRODUCTDESCRIPTION");
-                double productPrice = rs.getDouble("PRODUCTPRICE");
-                Product product = new Product(productId, productName, productStock, productDescription, productPrice);
-                products.add(product);
-            }
-        }
-        return products;
-    }
+//    public static List<Product> getProductsFromDatabase() throws SQLException {
+//        List<Product> products = new ArrayList<>();
+//        String query = "SELECT * FROM PRODUCTS";
+//        try (Connection conn = Setup.getDBConnection();
+//             PreparedStatement stmt = conn.prepareStatement(query);
+//             ResultSet rs = stmt.executeQuery()) {
+//            while (rs.next()) {
+//                String productId = rs.getString("PRODUCTID");
+//                String productName = rs.getString("PRODUCTNAME");
+//                int productStock = rs.getInt("PRODUCTSTOCK");
+//                String productDescription = rs.getString("PRODUCTDESCRIPTION");
+//                double productPrice = rs.getDouble("PRODUCTPRICE");
+//                Product product = new Product(productId, productName, productStock, productDescription, productPrice);
+//                products.add(product);
+//            }
+//        }
+//        return products;
+//    }
 
     // Update product stock in the database based on the items purchased
-    //Pass in a List of the items purchased (NEED ID
-    public static void updateStockAtCheckout (List<Product> itemList) {
-        String selectQuery = "SELECT PRODUCTSTOCK FROM PRODUCT WHERE PRODUCTID = ?";
-        String updateQuery = "UPDATE PRODUCT SET PRODUCTSTOCK = ? WHERE PRODUCTID = ?";
-        try (Connection conn = Setup.getDBConnection();
-             PreparedStatement selectStmt = conn.prepareStatement(selectQuery);
-             PreparedStatement updateStmt = conn.prepareStatement(updateQuery)) {
-
-            for (Product item : itemList) {
-                // Get current stock for the product
-                selectStmt.setString(1, item.getProductId());
-                ResultSet rs = selectStmt.executeQuery();
-                if (rs.next()) {
-                    int currentStock = rs.getInt("PRODUCTSTOCK");
-                    int newStock = currentStock - 1; // Subtract 1 for each purchased product
-                    updateStmt.setInt(1, newStock);
-                    updateStmt.setString(2, item.getProductId());
-                    updateStmt.executeUpdate();
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("Failed to update product stock in the database at checkout.");
-        }
-    }
-
-
-
-
-
-
-
+//    //Pass in a List of the items purchased (NEED ID
+//    public static void updateStockAtCheckout (List<Product> itemList) {
+//        String selectQuery = "SELECT PRODUCTSTOCK FROM PRODUCT WHERE PRODUCTID = ?";
+//        String updateQuery = "UPDATE PRODUCT SET PRODUCTSTOCK = ? WHERE PRODUCTID = ?";
+//        try (Connection conn = Setup.getDBConnection();
+//             PreparedStatement selectStmt = conn.prepareStatement(selectQuery);
+//             PreparedStatement updateStmt = conn.prepareStatement(updateQuery)) {
+//
+//            for (Product item : itemList) {
+//                // Get current stock for the product
+//                selectStmt.setString(1, item.getProductId());
+//                ResultSet rs = selectStmt.executeQuery();
+//                if (rs.next()) {
+//                    int currentStock = rs.getInt("PRODUCTSTOCK");
+//                    int newStock = currentStock - 1; // Subtract 1 for each purchased product
+//                    updateStmt.setInt(1, newStock);
+//                    updateStmt.setString(2, item.getProductId());
+//                    updateStmt.executeUpdate();
+//                }
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//            System.out.println("Failed to update product stock in the database at checkout.");
+//        }
+//    }
 
     // Update product details in the database when setters are called
     public void setProductName(String productName) {
@@ -150,36 +144,5 @@ public class Product {
             e.printStackTrace();
             System.out.println("Failed to update product price in the database.");
         }
-    }
 
-    public String getProductId() {
-        return productId;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public int getProductStock() {
-        return productStock;
-    }
-
-    public String getProductDescription() {
-        return productDescription;
-    }
-
-    public double getProductPrice() {
-        return productPrice;
-    }
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "productId='" + productId + '\'' +
-                ", productName='" + productName + '\'' +
-                ", productStock=" + productStock +
-                ", productDescription='" + productDescription + '\'' +
-                ", productPrice=" + productPrice +
-                '}';
-    }
 }
