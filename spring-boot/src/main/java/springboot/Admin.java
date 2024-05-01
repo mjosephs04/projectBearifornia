e712dc9c24c88574fd0ab1847266d2367ce91fa9
Oliver Liu <oliverliu100@gmail.com>
2024-04-30 23:28:22 -0500

    Move Admin methods to service

--- spring-boot/src/main/java/springboot/Admin.java
+++ spring-boot/src/main/java/springboot/Admin.java
@@ -1,17 +1,10 @@
 package springboot;
 
-import springboot.database.Setup;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
 public class Admin implements User{
     private static UserType classification = UserType.ADMIN;
     private String name;
     private String username;
     private String password;
-
 
     public Admin(String name){
         this.name = name;
@@ -35,35 +28,6 @@ public class Admin implements User{
     @Override
     public void setName(String x) {
         name = x;
-    }
-
-    public static String addUser(Connection conn, String name, String user, String pass, String userT){
-        String insertSQL = "INSERT INTO USERS (NAME, USERNAME, PASSWORD, USERTYPE) VALUES (?, ?, ?)";
-
-        try (PreparedStatement statement = conn.prepareStatement(insertSQL)) {
-            statement.setString(1, name);
-            statement.setString(2, user);
-            statement.setString(3, pass);
-            statement.setString(4, pass);
-            statement.executeUpdate();
-
-            return "success";
-        } catch(SQLException e) {
-            System.out.println("Could not insert users into database: " + e.getMessage());
-            return "Could not insert users into database: " + e.getMessage();
-        }
-    }
-
-    //returns either "success" or a fail message
-    public static String addClerk(String name, String username, String password, UserType type) {
-        Connection conn = Setup.getDBConnection();
-
-        if(type.equals(UserType.CLERK)){
-            return addUser(conn, name, username, password, type.toString());
-        }
-        else{
-            return "failure-- admins may only create CLERK accounts";
-        }
     }
 
     @Override
