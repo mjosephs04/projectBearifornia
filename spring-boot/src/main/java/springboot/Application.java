1d7412b0fa6c7753b3ffe0398d26da69be4df963
Oliver Liu <oliverliu100@gmail.com>
2024-04-30 23:33:53 -0500

    Modify Spring Boot annotation

--- spring-boot/src/main/java/springboot/Application.java
+++ spring-boot/src/main/java/springboot/Application.java
@@ -2,6 +2,7 @@ package springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import springboot.database.Setup;
 
 @SpringBootApplication
