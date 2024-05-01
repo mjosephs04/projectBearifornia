4f4a466d0376e5e7fe51a995856f8c44c0c5126e
Jake <117681409+Jacob-Gokale@users.noreply.github.com>
2024-04-30 20:52:24 -0500

    Adding dropdowns for add room page (#110)
    * Changed AddRoom to have dropdowns.
    
    * Changed AddRoom to have dropdowns.
    
    * Adding dropdowns to Clerk's add room page.
    
    * Adding dropdowns to Clerk's add room page.

--- frontend/src/Components/AddRoom/AddRoom.js
+++ frontend/src/Components/AddRoom/AddRoom.js
@@ -8,11 +8,16 @@ const AddRoom = (props) =>{
     const [error, setError] = useState(false)
     const [roomNumber, setRoomNumber] = useState([])
     const [roomCost, setRoomCost] = useState([])
-    const [roomType, setRoomType] = useState([])
-    const [numBeds, setNumBeds] = useState([])
-    const [qualityLevels, setQualityLevel] = useState([])
-    const [bedType, setBedType] = useState([])
-    const [smokingStatus, setSmokingStatus] = useState([])
+    const [roomType, setRoomType] = useState("Nature Retreat")
+    const [numBeds, setNumBeds] = useState("One Bed")
+    const [qualityLevels, setQualityLevel] = useState("Executive")
+    const [bedType, setBedType] = useState("Twin")
+    const [smokingStatus, setSmokingStatus] = useState(true)
+    // Handle changes in the dropdown
+    const handleChange = (event) => {
+        // Convert the string value to a boolean
+        setSmokingStatus(event.target.value === 'true');
+    };
     const navigate = useNavigate();
 
     const onButtonClick = () => {
@@ -34,10 +39,13 @@ const AddRoom = (props) =>{
                 if(response.status === 200){
                     navigate('/confirmation');
                 }
+
+
                 console.log(response);
                 return response.data;
             }).catch(error => {
-                console.log(error);
+                console.log("Error with authentication" + error);
+                setError(true);
         })
     };
 
@@ -64,51 +72,119 @@ const AddRoom = (props) =>{
             />
 
             <br/>
-
-                <input
-                    //value={email}
-                    placeholder="Enter Room Type"
-                    className={'inputBox'}
-                    onChange={evt => setRoomType(evt.target.value)}
-                />
             <br/>
 
-                <input
-                    //value={email}
-                    placeholder="Enter Number of Beds"
-                    className={'inputBox'}
-                    onChange={evt => setNumBeds(evt.target.value)}
-                />
+
+            <div className='addRoomContainer_01'>
+                <label className="LabelStyle_01">
+
+                    Select Room Type
+
+                    <select value={roomType} className="selectStyle_01" onChange={evt =>setRoomType(evt.target.value)}>
+
+                        <option value="Nature Retreat">Nature Retreat</option>
+
+                        <option value="Urban Elegance">Urban Elegance</option>
+
+                        <option value="Vintage Charm">Vintage Charm</option>
+
+                    </select>
+
+                </label>
+            </div>
+
 
             <br/>
-
-                <input
-                    //value={email}
-                    placeholder="Enter Quality Level"
-                    className={'inputBox'}
-                    onChange={evt => setQualityLevel(evt.target.value)}
-                />
-
             <br/>
 
-                <input
-                    //value={email}
-                    placeholder="Enter Bed Type"
-                    className={'inputBox'}
-                    onChange={evt => setBedType(evt.target.value)}
-                />
+
+            <div className='addRoomContainer_02'>
+                <label className="LabelStyle_02">
+
+                    Select Number of Beds
+
+                    <select value={numBeds} className="selectStyle_02" onChange={evt =>setNumBeds(evt.target.value)}>
+
+                        <option value="1">One Bed</option>
+
+                        <option value="2">Two Beds</option>
+
+                    </select>
+
+                </label>
+            </div>
+
+
+            <br/>
             <br/>
 
-                <input
-                    //value={email}
-                    placeholder="Smoking (true or false)"
-                    className={'inputBox'}
-                    onChange={evt => setSmokingStatus(evt.target.value)}
-                />
+
+            <div className='addRoomContainer_03'>
+                <label className="LabelStyle_03">
+
+                    Select Quality Level
+
+                    <select value={qualityLevels} className="selectStyle_03" onChange={evt =>setQualityLevel(evt.target.value)}>
+
+                        <option value="Executive Level">Executive Level</option>
+                        <option value="Business Level">Business Level</option>
+                        <option value="Comfort Level">Comfort Level</option>
+                        <option value="Economy Level">Economy Level</option>
+
+                    </select>
+
+                </label>
+            </div>
 
             <br/>
+            <br/>
+
+
+            <div className='addRoomContainer_04'>
+                <label className="LabelStyle_04">
+
+                    Select Bed Type
+
+                    <select value={bedType} className="selectStyle_04" onChange={evt =>setBedType(evt.target.value)}>
+
+                        <option value="Twin">Twin</option>
+                        <option value="Full">Full</option>
+                        <option value="Queen">Queen</option>
+                        <option value="King">King</option>
+
+                    </select>
+
+                </label>
+            </div>
+
+
+
+            <br/>
+            <br/>
+
+
+            <div className='addRoomContainer_05'>
+                <label className="labelStyle_05">
+                    Select Smoking Status
+                    <select select value={String(smokingStatus)} onChange={handleChange}>
+                        <option value="true">True</option>
+                        <option value="false">False</option>
+                    </select>
+                </label>
+            </div>
+
+
+            <br/>
+            <br/>
+
 
             <button className='add-room-button' onClick={onButtonClick}>Add Room</button>
+
+
+            {error && (
+                <h1 className='add-room-failed'>Adding Room Failed: A field is empty/bad input!</h1>
+            )}
+
 
         </div>
     );
