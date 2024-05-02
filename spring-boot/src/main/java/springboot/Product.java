79531c4d17787ca0764bed840bea4fd77f02fd51
Oliver Liu <oliverliu100@gmail.com>
2024-05-01 21:45:25 -0500

    Remove Setters and DB Update methods

--- spring-boot/src/main/java/springboot/Product.java
+++ spring-boot/src/main/java/springboot/Product.java
@@ -71,55 +71,6 @@ public class Product {
 //        }
 //    }
 
-    // Database update methods
-    private void updateProductNameInDatabase(String newName) {
-        String updateQuery = "UPDATE PRODUCTS SET PRODUCTNAME = ? WHERE PRODUCTID = ?";
-        try (Connection conn = Setup.getDBConnection(); PreparedStatement pstmt = conn.prepareStatement(updateQuery)) {
-            pstmt.setString(1, newName);
-            pstmt.setString(2, productId);
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("Failed to update product name in the database.");
-        }
-    }
-
-    private void updateProductStockInDatabase(int newStock) {
-        String updateQuery = "UPDATE PRODUCTS SET PRODUCTSTOCK = ? WHERE PRODUCTID = ?";
-        try (Connection conn = Setup.getDBConnection(); PreparedStatement pstmt = conn.prepareStatement(updateQuery)) {
-            pstmt.setInt(1, newStock);
-            pstmt.setString(2, productId);
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("Failed to update product stock in the database.");
-        }
-    }
-
-    private void updateProductDescriptionInDatabase(String newDescription) {
-        String updateQuery = "UPDATE PRODUCTS SET PRODUCTDESCRIPTION = ? WHERE PRODUCTID = ?";
-        try (Connection conn = Setup.getDBConnection(); PreparedStatement pstmt = conn.prepareStatement(updateQuery)) {
-            pstmt.setString(1, newDescription);
-            pstmt.setString(2, productId);
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("Failed to update product description in the database.");
-        }
-    }
-
-    private void updateProductPriceInDatabase(double newPrice) {
-        String updateQuery = "UPDATE PRODUCTS SET PRODUCTPRICE = ? WHERE PRODUCTID = ?";
-        try (Connection conn = Setup.getDBConnection(); PreparedStatement pstmt = conn.prepareStatement(updateQuery)) {
-            pstmt.setDouble(1, newPrice);
-            pstmt.setString(2, productId);
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("Failed to update product price in the database.");
-        }
-    }
-
     public String getProductId() {
         return productId;
     }
@@ -128,37 +79,16 @@ public class Product {
         return productName;
     }
 
-    // Update product details in the database when setters are called
-    public void setProductName(String productName) {
-        this.productName = productName;
-        updateProductNameInDatabase(productName);
-    }
-
     public int getProductStock() {
         return productStock;
-    }
-
-    public void setProductStock(int productStock) {
-        this.productStock = productStock;
-        updateProductStockInDatabase(productStock);
     }
 
     public String getProductDescription() {
         return productDescription;
     }
 
-    public void setProductDescription(String productDescription) {
-        this.productDescription = productDescription;
-        updateProductDescriptionInDatabase(productDescription);
-    }
-
     public double getProductPrice() {
         return productPrice;
-    }
-
-    public void setProductPrice(double productPrice) {
-        this.productPrice = productPrice;
-        updateProductPriceInDatabase(productPrice);
     }
 
     public String getImageURL() {
