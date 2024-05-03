0e71c6b5951cdb7acc801015b4fd7117ba0ec3e2
markjosephs <mjosephs@customsportssleeves.com>
2024-05-02 19:26:58 -0500

    logOut functionality

--- frontend/src/Components/Layout/Layout.js
+++ frontend/src/Components/Layout/Layout.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Logo from "../../bear_logo.png";
 import DropdownMenu from "../dropdownMenu/dropdownMenu";
 import './Layout.css'
 import {Link} from "react-router-dom";
+import axios from 'axios'
 
 const Layout = () => {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const checkLoggedIn = () => {
+        axios.get('http://localhost:8080/api/register/LOGGED-IN')
+            .then(response =>{
+                setIsLoggedIn(response.data);
+                console.log(response);
+                return response.data;
+            }).catch(error => {
+            console.log("Error: " + error);
+        })
+    };
+
+    useEffect(() => {
+        checkLoggedIn();
+    }, []); // Empty dependency array ensures useEffect runs only once on mount
+
+    const logOut = () => {
+        axios.post('http://localhost:8080/api/register/LOGGED-IN')
+            .then(response =>{
+                setIsLoggedIn(false);
+                return response.data;
+            }).catch(error => {
+            console.log("Error: " + error);
+        })
+    }
+
     return (
         <div className="container">
             <Link to='/' className="title-color">
@@ -40,7 +69,18 @@ const Layout = () => {
                     View Bill
                 </Link>
             </div>
-            <div className="login-drop"><DropdownMenu/></div>
+
+            {!isLoggedIn && (
+                <div className="login-drop"><DropdownMenu/></div>
+            )}
+
+            {isLoggedIn && (
+                <div>
+                    <button className='log-out-button' onClick=>Log Out</button>
+                </div>
+            )}
+
+
 
         </div>
     );
